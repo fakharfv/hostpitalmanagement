3defbecffc8c860599d059c23f9c063f21f23f10
fakharfv <fakharfv465@gmail.com>
2024-08-10 04:34:42 +0500

    sdd

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    input: 'public/assets/css/app.css',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'es',
-    },
-    // other configurations...
-  };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/assets/css/app.css', 'public/assets/js/app.js'],
+            input: ['resources/assets/css/app.css', 'resources/assets/js/app.js'],
             refresh: true,
         }),
     ],
