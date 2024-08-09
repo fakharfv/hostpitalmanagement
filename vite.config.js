be1637282ec22a2d4f8c353192a92f466576e6b5
fakharfv <fakharfv465@gmail.com>
2024-08-10 04:39:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/assets/css/style.css', 'resources/assets/js/main.js'],
+            input: ['public/assets/css/style.css', 'public/assets/js/main.js'],
             refresh: true,
         }),
     ],
